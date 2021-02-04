ff7ebe892cf7d4da6ee27a0745db702ffeadce86
Adrian Vazquez <aavm0000@gmail.com>
2021-02-04 13:00:37 -0400

    chore: exception inheritance wdt

--- src/watchdog.py
+++ src/watchdog.py
@@ -2,8 +2,10 @@ from threading import Timer
 import os
 import sys
 
-class Watchdog(BaseException):
+class Watchdog(Exception):
     def __init__(self, timeout, userHandler=None):  # timeout in seconds
+        message = "Error wdt"
+        super(Exception, self).__init__(message)
         self.timeout = timeout
         self.handler = userHandler if userHandler is not None else self.defaultHandler
         self.timer = Timer(self.timeout, self.handler)

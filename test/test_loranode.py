03cb89831848dbe01f0b58a5cde2a9474b24b064
Adrian Vazquez <aavm0000@gmail.com>
2021-02-25 12:20:35 -0400

    test: lora node test members

---
+++ test/test_loranode.py
@@ -0,0 +1,23 @@
+from src.centralnode import *
+import unittest
+test_dic = { 
+        "loraid":2,
+        "slaves":[1,2,3,4,5,6]
+        }
+class TestLoraNode(unittest.TestCase):
+    """
+    Test the loranode members
+    """
+    def test_init(self):
+        test_dic["slaves"]= 10*test_dic["slaves"]
+        test_lora = loranode(test_dic)
+        self.assertEqual(test_lora.lastpollsize, 8, "Must be equal")
+    
+    def test_quantity_poll(self):
+        test_lora = loranode(test_dic)
+        n = test_lora.quantity_poll()
+        self.assertEqual(n, 3, "Must be equal")
+
+        
+if __name__ == "__main__":
+    unittest.main()
